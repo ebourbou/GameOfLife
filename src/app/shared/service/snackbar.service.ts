caec88f1c711ee72c2ed9d7a42716da43479f7d8
strato <ebourbou@gmail.com>
2020-11-08 17:30:43 +0100

    moved and updated pattern-service and mock and util to shared. itroduced snackbar-service

---
+++ src/app/shared/service/snackbar.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SnackbarService {
+  constructor(private snackBar: MatSnackBar) {}
+
+  info(message: string): void {
+    this.snackBar.open(message, 'Schliessen', { duration: 2000 });
+  }
+
+  error(message: string): void {
+    this.snackBar.open(message, 'Schliessen');
+  }
+
+  errors(message: string, details: string[]): void {
+    this.snackBar.open([message].concat(details).join('\n'), 'Schliessen');
+  }
+}
