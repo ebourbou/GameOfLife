0e4f6ad44707af801e88455b1d22e493afd3ab39
Jochen <jochen.reger@hsr.ch>
2021-01-03 22:06:25 +0100

    Another round on testing login

--- src/app/auth/account.module.spec.ts
+++ src/app/auth/account.module.spec.ts
@@ -1,14 +1,17 @@
 import 'jasmine';
-import { async, inject, TestBed, TestBedStatic } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, inject, TestBed, TestBedStatic, tick } from '@angular/core/testing';
 import { UserService } from '../users/services/users.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
-import { AmplifyService } from 'aws-amplify-angular';
+import { FormsModule, NgForm } from '@angular/forms';
+import { AmplifyService, FormComponent } from 'aws-amplify-angular';
 import { AuthService } from '../core/services/auth.service';
 import { NotificationService } from '../shared/service/notification.service';
 import { AuthServiceMock } from '../core/services/auth.service.mock';
 import { BehaviorSubject, of } from 'rxjs';
+import { DebugElement } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { By } from 'protractor';
 
 class RouterStub {
   navigateByUrl(url: string): string {
@@ -17,6 +20,11 @@ class RouterStub {
 }
 
 describe('Component: Account', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let debug: DebugElement;
+  let el: HTMLElement;
+
   beforeEach(async(() => {
     const MockAmplifyService = TestBed.configureTestingModule({ providers: [AmplifyService] });
     const MockNotificationService = TestBed.configureTestingModule({ providers: [NotificationService] });
@@ -32,83 +40,33 @@ describe('Component: Account', () => {
 
     TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      imports: [FormsModule],
+      imports: [BrowserModule, FormsModule],
       providers: [
         { provide: AmplifyService, userClass: MockAmplifyService },
-        { provide: ActivatedRoute, useValue: activatedRouteStub },
-        { provide: Router, useClass: Router },
+        { provide: ActivatedRoute, useValue: { snapshot: { queryParams: { returnUrl: '' } } } },
         { provide: AuthService, useClass: AuthServiceMock },
         { provide: NotificationService, useClass: MockNotificationService },
         { provide: UserService, useClass: MockUserService },
         { provide: Router, useClass: RouterStub },
       ],
-    });
-  }));
-  it('should create an instance', inject(
-    [Router, UserService],
-    (
-      amplifyService: AmplifyService,
-      activatedRoute: ActivatedRoute,
-      router: Router,
-      authService: AuthService,
-      notificationService: NotificationService
-    ) => {
-      const component = new LoginComponent(amplifyService, activatedRoute, router, authService, notificationService);
-
-      expect(component).toBeTruthy();
-    }
-  ));
-});
-
-describe('when the login page loads', () => {
-  it('then the login name should be defaulted', inject(
-    [AmplifyService, ActivatedRoute, Router, AuthService, NotificationService],
-    (
-      amplifyService: AmplifyService,
-      activatedRoute: ActivatedRoute,
-      router: Router,
-      authService: AuthService,
-      notificationService: NotificationService
-    ) => {
-      const component = new LoginComponent(amplifyService, activatedRoute, router, authService, notificationService);
-      component.ngOnInit();
-
-      it('form invalid when empty', () => {
-        expect(component.form.valid).toBeFalsy();
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        //   debug = fixture.debugElement.query(By.css('form'));
+        //   el = debug.nativeElement;
       });
+  }));
 
-      component.form.controls.login.setValue('user');
-      component.form.controls.password.setValue('wrong_password');
+  it('testing form the proper way', fakeAsync(() => {
+    // This first detectChanges is necessary to properly set up the form
+    fixture.detectChanges();
 
-      // expect(component.form.login.username).toEqual('');
-    }
-  ));
+    // Tick needs to be called in order for form controls to be registered properly.
+    tick();
+    component.form.controls.username.setValue('user');
+    component.form.controls.password.setValue('wrong_password');
+
+  }));
 });
-/*  ));
-     it('then the error message should not be displayed', inject([Router, UserService], (router: Router, userService: UserService) => {
-        let component = new LoginComponent(router, userService);
-        expect(component.showErrorMessage).toBe(false);
-      }));
-});
-
-describe('when a valid username and password are entered', () => {
-    it('then the home route should be displayed', inject([Router, UserService], (router: Router, userService: UserService) => {
-      spyOn(userService, 'login').and.returnValue(Observable.of(true));
-      spyOn(router, 'navigateByUrl').and.returnValue('');
-      let component = new LoginComponent(router, userService);
-      component.authenticateUser();
-      expect(router.navigateByUrl).toHaveBeenCalled();
-    }));
-  });
-  describe('when an invalid username and password are entered', () => {
-    it('then Login Failed should be displayed', inject([Router, UserService], (router: Router, userService: UserService) => {
-      spyOn(userService, 'login').and.returnValue(Observable.of(false));
-      spyOn(router, 'navigateByUrl').and.returnValue('');
-      let component = new LoginComponent(router, userService);
-      component.authenticateUser();
-      expect(router.navigateByUrl).not.toHaveBeenCalled();
-      expect(component.showErrorMessage).toBe(true);
-    }));
-  });
-
-*/
